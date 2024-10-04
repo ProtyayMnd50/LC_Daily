d53121006d96a11222d17a9475bd52c2d4146577
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2024-10-05 00:09:28 +0530

    Time: 224 ms (5.11%) | Memory: 81.4 MB (5.11%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
+    int md(int x,int k){
+        if(x>=0)return x%k;
+        else if(x>=-k&&x<0){
+            return (x+k)%k;
+        }else{
+            int q=abs(x)/k;
+            int cl=(q+1)*-1*k;
+            return (x-cl)%k;
+        }
+
+    }
     bool canArrange(vector<int>& arr, int k) {
-        vector<int> freq(k, 0);
-        
-        for (int num : arr) {
-            int remainder = ((num % k) + k) % k;
-            freq[remainder]++;
-        }
-   
-        if (freq[0] % 2 != 0) return false; 
-        
-        for (int i = 1; i <= k / 2; i++) {
-            if (freq[i] != freq[k - i]) return false; 
-        }
-        
-        return true;
+       map<int,int>mp;
+       for(int i=0;i<arr.size();i++)
+       {
+        int modul=md(arr[i],k);
+        mp[modul]++;
+       }
+       for(int i=1;i<k;i++){
+        if(mp[i]!=mp[k-i])return false;
+       }
+       if(mp[0]&1)return false;
+
+       return true;
     }
 };
